20f4f8229b13b1d7c3b2b7a0c9da8a895c37a4bd
JunaidSumsaal <junaidsumsaal@gmail.com>
2025-03-13 08:13:26 +0300

    hotfix

--- quickcart/settings.py
+++ quickcart/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'theme', 'static_src'),
+    # os.path.join(BASE_DIR, 'theme', 'static_src'),
     os.path.join(BASE_DIR, 'theme', 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
